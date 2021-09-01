b1330a6e498162f80ccaa73824bc2d77be43fae3
Piyush Rathi <piyushshrathi@gmail.com>
2021-09-01 17:15:03 +0530

    Second Commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,8 +9,18 @@ namespace ConsoleApp
         {
             Console.WriteLine("Hello World!");
             BookStoreRepository repository = new BookStoreRepository();
+
+            //For- GetAllBooks() method
             var books = repository.GetAllBooks();
             foreach (var book in books) {
+                Console.WriteLine(book.Title);
+            }
+
+            //For- GetAllBooks() method
+            int id = 1;
+            var b = repository.GetBook();
+            foreach (var book in b)
+            {
                 Console.WriteLine(book.Title);
             }
         }
